8997a065ba5a437e8484d0226da6fe78eb486f3d
Ishan Dassanayake <ishansd94@gmail.com>
2018-02-02 14:26:11 +0530

    Add users, roles, permissions views

--- resources/views/layouts/includes/styles.blade.php
+++ resources/views/layouts/includes/styles.blade.php
@@ -7,6 +7,9 @@
 {{--jQuery data-tables--}}
 <link rel="stylesheet" href="{{ asset('css/data-tables.min.css') }}">
 
+{{--ballon tool tips--}}
+<link rel="stylesheet" href="{{ asset('css/balloon.min.css') }}">
+
 {{--custom styles--}}
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('css/media.styles.css') }}">
