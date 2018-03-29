2a8d0157fcd05d9af2860a78c1da8256feacd805
Chamara Singhabahu <mails.chamara@gmail.com>
2018-03-29 14:08:03 +0530

    Enable HTTPS on script asserts

--- resources/views/layouts/includes/scripts.blade.php
+++ resources/views/layouts/includes/scripts.blade.php
@@ -1,9 +1,9 @@
 {{--jQuery and mdbootstrap--}}
-<script type="text/javascript" src="{{ asset('js/compiled.min.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('js/compiled.min.js') }}"></script>
 
 {{--jQuery data-tables--}}
-<script type="text/javascript" src="{{ asset('js/data-tables.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/data-tables.bootstrap4.min.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('js/data-tables.min.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('js/data-tables.bootstrap4.min.js') }}"></script>
 
 
 {{--JjQuery custom column --}}
@@ -16,6 +16,6 @@
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
 
 {{--custom scripts--}}
-<script type="text/javascript" src="{{ asset('js/scripts.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('js/scripts.js') }}"></script>
 
 
